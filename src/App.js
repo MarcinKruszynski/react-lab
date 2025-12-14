a049e7161127c493c9ef8bd8b01f203cfa2d8ae3
MarcinKruszynski <martin.kruszynski@gmail.com>
2025-12-14 11:42:21 +0100

    feat: add logical conditions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let title = 'Wall-E';
+    const [title, setTitle] = useState('Wall-E');
+
+
+    let message = '';     
+    
+    if (title.length <= 5)
+      message = "tekst jest za krótki";
+    else if (title.length >= 15)
+      message = "tekst jest za długi";
+    else
+      message = "tekst ma odpowiednia długość";  
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value);
+        //console.log(event.target.value);              
     }
 
     return (
       <div>
           <h1>My favourite movies to watch</h1>
           <h2>My favourite movie for today is {title}</h2>
+          {
+            title.length > 0 && <div>{message}</div>
+          }
           <input type="text" onChange={handleChange}/>
       </div>
     );
